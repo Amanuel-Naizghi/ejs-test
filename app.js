4f2b62aabeb56ab1c5340b334a82507fe2a15e1a
Amanuel-Naizghi <naizghiaman@gmail.com>
2025-06-10 06:57:48 +0300

    Common footer has been added to both pages using include and a static style has been applied to the footer

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-
 const path = require("node:path");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 const PATH = 3000;
+
+app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
     res.render("index", {message: "EJS rocks!"});
